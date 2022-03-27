bcffc11e667d67d9bfda17904c11d3d057472a4d
hannahnmcdonald <hannahnmcdonald@protonmail.com>
2022-03-27 19:28:49 -0400

    login/logout methods

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -19,23 +19,38 @@
 </template>
 
 <script>
-
-  export default {
-    name: 'app',
-    data () {
-      return {
-        user: null
-      }
+export default {
+  name: 'app',
+  data() {
+    return {
+      user: null
+    }
+  },
+  async created() {
+    await this.refreshUser()
+  },
+  watch: {
+    '$route': 'onRouteChange'
+  },
+  methods: {
+    login() {
+      this.$auth.loginRedirect()
     },
-    methods: {
-      login () {
-
-      },
-      async logout () {
-
-      }
+    async onRouteChange() {
+      // every time a route is changed refresh the user details
+      await this.refreshUser()
+    },
+    async refreshUser() {
+      // get new user details and store it to user object
+      this.user = await this.$auth.getUser()
+    },
+    async logout() {
+      await this.$auth.logout()
+      await this.refreshUser()
+      this.$router.push('/')
     }
   }
+}
 </script>
 
 <style>
